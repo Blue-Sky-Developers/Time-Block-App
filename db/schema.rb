5e5e9f543de9b67e126ed92a9fb58ef0ea5ebbd4
Arjan Patel <111463230+arjanpatel@users.noreply.github.com>
2024-04-11 19:05:38 -0400

    Updated Calendar HTML

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_26_074028) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_10_231852) do
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "password_digest"
@@ -18,4 +28,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_26_074028) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "events", "users"
 end
