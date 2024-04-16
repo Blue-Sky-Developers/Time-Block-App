f16e5c6e8062998dcfe48bdb5d36d891f9a21aea
thytran <ttran140201@gmail.com>
2024-04-15 23:37:42 -0400

    delete-statement-test

--- db/statement_test.rb
+++
@@ -1,7 +0,0 @@
-# Test case to ensure statement coverage for User model
-describe User do
-    it "creates a new user" do
-      user = User.new(email: "test@example.com", password: "password")
-      expect(user.save).to be true
-    end
-  end
