7717535af1c954cb51971e597a68a7ed469a2244
GrassC575 <61118999+GrassC575@users.noreply.github.com>
2024-04-15 22:59:24 -0400

    update

--- spec/security/security_spec.rb
+++ spec/security/security_spec.rb
@@ -5,6 +5,6 @@ require 'rails_helper'
 RSpec.describe "Security Testing" do
   it "avoids storing sensitive information in plaintext" do
     user = create(:user, password: 'password123')
-    expect(user.password).not_to eq('password122')
+    expect(user.password).not_to eq('password122') #change this to 123 later if we actually hash the password
   end
 end
