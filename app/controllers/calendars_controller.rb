2d7211d6db2c4f0d21ce7c93c29669f70979034a
shrey-pansuriya <shreypansuriya11@gmail.com>
2024-04-21 12:26:20 -0400

    assa

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -2,10 +2,10 @@ class CalendarsController < ApplicationController
     before_action :require_login
 
     def show
-        start_date = Date.today
-        end_date = start_date.end_of_week
-        @events = current_user.events.where(start_time: start_date.beginning_of_day..end_date.end_of_day)
-      end      
+        start_date = params.fetch(:start_date, Date.today).to_date
+        # Use Current.user instead of current_user
+        @events = Current.user.events.where(start_time: start_date.beginning_of_week..start_date.end_of_week)
+    end
 
     def require_login
         unless session[:user_id] && User.exists?(session[:user_id])
